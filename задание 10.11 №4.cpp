1395f1464aa2ce501d0dfce2e0490730f6da2a2d
DIREKTOR228PLAY <150294748+DIREKTOR228PLAY@users.noreply.github.com>
2023-11-14 10:39:38 +0300

    Add files via upload

---
+++ задание 10.11 №4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int sum = 0;
+
+    for (int i = 1; i <= 10; i++) {
+        sum += i * 17;
+
+
+    }
+
+    cout << "Ответ: " << sum << endl;
+
+    return 0;
+}
